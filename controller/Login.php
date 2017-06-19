e1e00b4fb8d6e258777ac47efa1d85b8dc328d6e
cachu <gcachu.o@gmail.com>
2017-06-18 21:01:56 -0400

    init

---
+++ controller/Login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cachu
+ * Date: 18/jun/2017
+ * Time: 08:28 PM
+ */
+
+namespace controller;
+
+
+class Login
+{
+    public $_nombre;
+
+    function __construct()
+    {
+        $this->_nombre = "memo";
+    }
+}
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cachu
+ * Date: 18/jun/2017
+ * Time: 08:29 PM
+ */
+
+new index();
+
+class index
+{
+    private $_view, $_controller;
+
+    function __construct()
+    {
+        if (empty($_GET)) {
+            header("Location: /?m=login");
+            exit;
+        }
+        $name = $_GET['m'];
+        $this->_controller = $this->callController($name);
+        $this->_view = $this->callView($name);
+        print $this->_view;
+    }
+
+    function callController($name)
+    {
+        require_once "controller/$name.php";
+        $nameController = "controller\\$name";
+        return new $nameController();
+    }
+
+    function callView($view)
+    {
+        $controller=$this->_controller;
+        ob_start();
+        require_once "view/$view.phtml";
+        $View = ob_get_clean();
+        return $View;
+    }
+}
