14c34775106055c95778f8688130d248662f712f
cachu <gcachu.o@gmail.com>
2017-06-18 22:34:18 -0400

    error handling

--- index.php
+++ index.php
@@ -14,33 +14,60 @@ class index
 
     function __construct()
     {
-        if (empty($_GET)) {
-            header("Location: /?m=login");
-            exit;
+        try {
+            register_shutdown_function([$this, "fatal_handler"]);
+            if (empty($_GET)) {
+                header("Location: /?m=login");
+                exit;
+            }
+            $name = $_GET['m'];
+            $this->_controller = $this->callController($name);
+            $this->_view = $this->callView($name);
+            print $this->_view;
+        } catch (Exception $ex) {
+            print $ex->getMessage();
         }
-        $name = $_GET['m'];
-        $this->_controller = $this->callController($name);
-        $this->_view = $this->callView($name);
-        print $this->_view;
     }
 
     function callController($name)
     {
-        require_once "controller/$name.php";
-        $nameController = "controller\\$name";
-        return new $nameController();
+        if (file_exists("controller/$name.php")) {
+            include_once "controller/$name.php";
+            $nameController = "controller\\$name";
+            return new $nameController();
+        } else return null;
     }
 
     function callView($view)
     {
         $controller = $this->_controller;
         ob_start();
-        require_once "view/$view.phtml";
-        $body = ob_get_contents();
-        ob_end_clean();
-        require_once "view/template.phtml";
+        if (file_exists("view/$view.phtml")) {
+            include_once "view/$view.phtml";
+            $body = ob_get_contents();
+            ob_end_clean();
+        } else $body = "La pagina no existe";
+        include_once "view/template.phtml";
         $View = ob_get_contents();
         ob_end_clean();
         return $View;
+    }
+
+    function fatal_handler()
+    {
+        $errfile = "unknown file";
+        $errstr = "shutdown";
+        $errno = E_CORE_ERROR;
+        $errline = 0;
+
+        $error = error_get_last();
+
+        if ($error !== NULL) {
+            $errno = $error["type"];
+            $errfile = addslashes($error["file"]);
+            $errline = $error["line"];
+            $errstr = "[$errline][$errfile] " . preg_replace("/\r|\n/", " ", addslashes($error["message"]));
+            print "<script>console.error('$errstr')</script>";
+        }
     }
 }
